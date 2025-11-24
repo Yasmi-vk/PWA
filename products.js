9b1b1e0b4d962a54d97cbe98d2ebf7e8802db8d3
Yasmi Koticha <YK336@live.mdx.ac.uk>
2025-11-24 11:55:43 +0400

    Added all files

---
+++ products.js
@@ -0,0 +1,56 @@
+let products = [
+    {
+        "id": 1001,
+        "title": "Cat food, 25lb Bag",
+        "description": "A 25 pound bag of <em>irresistible</em>, organic goodness for you",
+        "price": 20.00,
+        "image": "stich pic.jpg",
+        "availableInventory": 10,
+        "rating": 2
+    },
+    {
+        "id": 1002,
+        "title": "Yarn",
+        "description": "Yarn your cat can play with",
+        "price": 20.00,
+        "image": "stich pic.jpg",
+        "availableInventory": 10,
+        "rating": 2
+    },
+    {
+        "id": 1002,
+        "title": "Dog food, 25lb bag",
+        "description": "A 25 pound bag of <em>irresistable</em>, organic goodness for you",
+        "price": 30.00,
+        "image": "stich Pic.jpg",
+        "availableInventory": 10,
+        "rating": 2
+    },
+     {
+        "id": 1003,
+        "title": "Fish food, 25lb bag",
+        "description": "A 25 pound bag of <em>irresistable</em>, organic goodness for you",
+        "price": 40.00,
+        "image": "stich Pic.jpg",
+        "availableInventory": 10,
+        "rating": 2
+    },
+     {
+        "id": 1004,
+        "title": "Lion food, 25lb bag",
+        "description": "A 25 pound bag of <em>irresistable</em>, organic goodness for you",
+        "price": 50.00,
+        "image": "stich Pic.jpg",
+        "availableInventory": 10,
+        "rating": 2
+    },
+    {
+        "id": 1005,
+        "title": "Lamp food, 25lb bag",
+        "description": "A 25 pound bag of <em>irresistable</em>, organic goodness for you",
+        "price": 60.00,
+        "image": "stich Pic.jpg",
+        "availableInventory": 10,
+        "rating": 2
+    }
+]
---
+++ service-worker.js
@@ -0,0 +1,29 @@
+var cacheName = 'petstore-v1';
+var cacheFiles = [
+    'index.html',
+    'product,js',
+    'petstore.webmanifest',
+    'image/stich pic.jpg'
+]
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            console.log('[Service Worker] caching all the files');
+            return cache.addAll(cacheFiles);
+        })
+    );
+});
+self.addEventListener('fetch', function (e) {
+    e.responsdWith(
+        caches.match(e.request).then(function (r) {
+              return r || fetch(e.request).then(function (response){
+                return caches.open(cacheName).then(function (cache){
+                    cache.put(e.request, response.clone());
+                    return response;
+                });
+              });
+        })
+    );
+});
+ 
